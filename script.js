a9fdd2f8e74cd823fc3ca7e5c82993caa9493ac2
JustAbdou <bennabet2001@gmail.com>
2024-09-08 21:33:06 +0100

    fixed a javascript error

--- script.js
+++ script.js
@@ -4,13 +4,3 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
-
-
-function window.open(path, filename) {
-  const link = document.createElement('a');
-  link.href = path; // Path to your local certificate
-  link.download = filename;   // Suggested file name for download
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-}
